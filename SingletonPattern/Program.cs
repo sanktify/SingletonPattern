f77fa49ed512cb4d6f5687c2dbb705d153395ac2
Sanchit <sanchit1295@gmail.com>
2019-03-03 19:02:18 +0530

    Singleton Pattern Example
    Creating a singleton pattern - one instance of the class at all times

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton obj = Singleton.Instance;
+            obj.DisplayValues("this is from Program class");
+        }
+    }
+}
---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                };
+                return instance;
+            }
+        }
+
+        public void DisplayValues(string input)
+        {
+            Console.WriteLine(input); 
+        }
+    }
+}
